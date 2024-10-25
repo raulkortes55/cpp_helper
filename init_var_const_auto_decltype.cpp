d2cc21c41b0b81d037f6b0b91975da3e6ea196f5
raulkortes55 <raulkortes55@gmail.com>
2024-10-25 16:51:27 +0300

    Update init_var_const_auto_decltype.cpp
    auto_decltype_const_final

--- init_var_const_auto_decltype.cpp
+++ init_var_const_auto_decltype.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <list> // двусвязные списки  two linked list
 using std::cout;
 using std::endl;
 
 
+//void func(auto x) {}  ERROR with AUTO  лучше вместо использовать перегрузку или определять шаблон FUNC
+
+
+
 int main() {
+    std::list<short> dg = {-3,-2,0,2,3};
+
+    for (std::list<short>::iterator i = dg.begin(); i!= dg.end(); ++i)
+        cout << *i << " ";
+
+    //BUT WE CAN DO SO auto instead of std::list<short>::iterator
+    for (auto i = dg.begin(); i!= dg.end(); ++i)
+        cout << *i << " ";
+
+    
+
+
+    /*
     int val = 0; //init 
     int pow[] = {1,2,4,8}; //init 
 
@@ -26,8 +44,58 @@ int main() {
     double p (1 * 3.4 - 1);
     bool n_f1(false), t_f1(true);
 
+    //CONST must be initialized!
+    const double pi {3.1415};
+
+    //auto instead of auto comiler insert calculatedd type of RHS
+    auto ka = -100; //auto int
+    auto da = 76.98; //auto double
+    auto ga = 0.55f; //auto float
+    auto ha = 'f'; //auto char
+
+    auto hh = (short)10 + 1000000L; //auto unsigned long
+    auto ii = 1 +2 -10 ; //auto int
+    auto dda = 1 + ii +3.0; // auto double
+    auto gg = 55u;//auto unsigned int
+
+    */
+
+    /*
+    int *ptr = nullptr;
+    int k;
+    int& lk = k;
+
+    auto t1 = k; //int
+    auto t2 = *ptr; //int
+    auto t3 = ptr; //int *
+    auto t4 = &ptr; //int **
+    auto t5 = lk; //int
+    */
+
+   /*
+    const int *ptr = nullptr;
+    const int k = 0;
+    const int& lk = k;
+
+    auto t1 = k; //int
+    auto t2 = *ptr; //int
+    auto t3 = ptr; // const int *
+    auto t4 = &ptr; //const int **
+    auto t5 = lk; //int
+
+
+    decltype(k) var1 = 1; //return type  wo changes
+    decltype(ptr) var2; //const int *
+    decltype(lk) var3 = k; // const int &
+
+    
+
+
+
     cout << sh << " " << d << endl;
     cout << ch << " " << lv << endl;
+
+    */
 
     return 0;
 }
